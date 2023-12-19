7272deb59a94164d73ce3c9e1df0cda2656c9bef
Nils Ottosson <info@noidea.se>
2023-12-19 20:15:39 +0100

    upg3

--- SJ22POSU_EnterpriseJavaFrontend/Inlamningsuppgift3/script.js
+++ SJ22POSU_EnterpriseJavaFrontend/Inlamningsuppgift3/script.js
@@ -1,32 +1,36 @@
 const nums = document.querySelectorAll("input")
 const form = document.querySelector("form")
-const fademsg = document.querySelector('.fade-out-element');
 const button1 = document.querySelector("#btnn1")
 const button2 = document.querySelector("#btnn2")
 const button3 = document.querySelector("#btnn3")
-const alermsg = document.querySelector("#alermsg")
+//const alermsg = document.querySelector("#alermsg-success")
+//const alermsg = document.querySelector("#alermsg-warning")
 const msglog = document.querySelector("#log")
 const pinform = document.querySelector("#pinform")
 const date = new Date();
 const pin = "1qaz";
+let alermsg;
 let inpin = "";
 let larmStatus = 1;
 let audio = new Audio();
+let opacity = 0;
+let intervalID = 0;
 
 
 
 const firelarm = () => {
     console.log("larmljud", inpin)
     audio.src = "emergency-alarm-with-reverb-29431.mp3";
-    //audio.play()
+    audio.play()
+
     msgHandler("FIREALRM")
-   
+
 }
 
 const thieflarm = () => {
     console.log("larmljud", larmStatus)
     audio.src = "clock-alarm-8761.mp3";
-    //audio.play()
+    audio.play()
     msgHandler("THIEFLARM")
 }
 
@@ -39,23 +43,34 @@ const larm = () => {
 
 
 const msgHandler = (msg) => {
-    msglog.innerHTML +=  "<h6>" + date + " -- " + msg + "</h6>";
+    msglog.innerHTML += "<h6>" + date + " -- " + msg + "</h6>";
+}
+
+const show = () => {
+    //var body = document.getElementById("body"); 
+    opacity = Number(window.getComputedStyle(alermsg)
+        .getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        alermsg.style.opacity = opacity
+    } else {
+        clearInterval(intervalID);
+    }
+
 }
 
 
-
-const checkPin = () => {
+//  mmove to loopPin
+/* const checkPin = () => {
     if (pin === inpin) {
         button1.disabled = true
         button2.disabled = true
         button3.disabled = true
         msgHandler("LARM function is INACTIVAT")
-    }else
+    } else
         msgHandler("LARM function is still ACTIV")
 
-
-
-}
+} */
 
 
 const loopPin = nums.forEach((num, index) => {
@@ -66,20 +81,33 @@ const loopPin = nums.forEach((num, index) => {
     num.addEventListener('keyup', () => {
         if (num.value.length === 1) {
             inpin += num.value
-            
-            checkPin()
+
             console.log(nums[nums.length - 1].value.length)
             if (nums[nums.length - 1].value.length == 1) {
-            //alert('Hello there, sdsdsI am being submitted');
-            
-            }   
+                //alert('Hello there, sdsdsI am being submitted');
+                //checkPin()
+                if (pin === inpin) {
+                    button1.disabled = true
+                    button2.disabled = true
+                    button3.disabled = true
+                    alermsg = document.querySelector("#alermsg-success")
+                    msgHandler("LARM function is INACTIVAT")
+
+                } else {
+                    alermsg = document.querySelector("#alermsg-warning")
+                    msgHandler("LARM function is still ACTIV")
+                }
+                setInterval(show, 100);
+
+            }
             //console.log(nums[nums.length - 1].value.length)
             nums[parseInt(num.dataset.id) + 1].focus()
-            
+
 
         }
 
     })
+    
 })
 
 
